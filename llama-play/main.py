79fa3c3d5a743cf14261dc31fe17400711a068b7
D-YOGA-2024\dekay <dekay01@hotmail.com>
2025-05-31 13:21:04 -0400

    FEATURE: Add configuration and Non Streaming Chat Analyzer Option

--- llama-play/main.py
+++ llama-play/main.py
@@ -1,11 +1,14 @@
 import src.analyzers as analyzer
+import src.llama_config as configuration
 import src.llama_logging as logging
 
+
 def main():
-    config = {
-        "log_level": "INFO",
-    }
-    logger = logging.configure(config, analyzer.LlamaChat('llama3.1'))
+    config = configuration.generate_config()
+    stream = config.get('stream')
+    model = config.get('model')
+    log_analyzer = analyzer.create(model, stream)
+    logger = logging.configure(config, log_analyzer)
     logger.info("Logging configured successfully.")
 
 
